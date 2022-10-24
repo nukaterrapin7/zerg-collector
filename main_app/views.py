fe6a436c3fadb75ae83e57b3b60dbd9e0f9ff343
nukaterrapin7 <pythonism007@gmail.com>
2022-10-24 18:37:36 -0500

    Adds ORM and admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Zerg
 
 # Create your views here.
 
-class Zerg:
-    def __init__(self, name, description, minerals, vespene):
-        self.name = name
-        self.description = description
-        self.minerals = minerals
-        self.vespene = vespene
-
-zergs = [
-    Zerg('Zergling', 'Small melee unit.', 50, 0),
-    Zerg('Roach', 'Small range unit', 75, 25),
-    Zerg('Ultralisk', 'Large melee unit', 300, 200)
-]
-
 def home(request):
-    return HttpResponse('<h1>Welcome to The Swarm</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def zergs_index(request):
-    return render(request, 'zergs/index.html', { 'zergs': zergs})
+    zergs = Zerg.objects.all()
+    return render(request, 'zergs/index.html', { 'zergs': zergs })
