222fe79dc103cc9e9d4353151d4d7f03935da543
nukaterrapin7 <pythonism007@gmail.com>
2022-10-28 11:45:30 -0500

    adds Essence model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Zerg
+from .models import Zerg, Essence
 
 # Register your models here.
 
 admin.site.register(Zerg)
+admin.site.register(Essence)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+ABSORBTIONS = (
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+)
+
 # Create your models here.
 
 class Zerg(models.Model):
@@ -14,3 +20,16 @@ class Zerg(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'zerg_id': self.id})
+
+class Essence(models.Model):
+    date = models.DateField('absorbtion date')
+    amount = models.CharField(
+        max_length=1,
+        choices=ABSORBTIONS,
+        default=ABSORBTIONS[0][0]
+    )
+
+    zerg = models.ForeignKey(Zerg, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_absorbtion_display()} on {self.date}"
